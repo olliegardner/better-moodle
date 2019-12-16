134becc9481373271a8ecb86460d17a6f5a7e410
olliegardner <olliegardner@gmail.com>
2019-12-16 23:52:13 +0000

    added config

--- submission.py
+++ submission.py
@@ -1,8 +1,25 @@
-from requests import Session
+from requests import session
 from bs4 import BeautifulSoup as bs
+import configparser
 
-LOGIN_URL = "https://moodle.gla.ac.uk/login/index.php"
+config = configparser.RawConfigParser()
+config.read("config.conf")
 
-with Session() as s:
-    site = s.get(LOGIN_URL)
-    print(site.content)
+USERNAME = config.get("better-moodle", "username")
+PASSWORD = config.get("better-moodle", "password")
+LOGIN_URL = config.get("better-moodle", "login_url")
+
+with session() as ses:
+    site = ses.get(LOGIN_URL)
+    
+    content = bs(site.content, "html.parser")
+    
+    token = content.find("input", {"name": "logintoken"})["value"]
+
+    data = {"username": USERNAME, "password": PASSWORD, "logintoken": token}
+
+    ses.post(LOGIN_URL, data)
+
+    dashboard = ses.get("https://moodle.gla.ac.uk/my/")
+
+    print(dashboard.content)
